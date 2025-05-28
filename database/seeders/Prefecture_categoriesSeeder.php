3b9c58edb072ca3d2aa5666706758eb0f91c8ae7
kitaura <s.kitaura@core-tech.jp>
2025-05-28 16:18:17 +0900

    prefectureCategoriesSeederを二次元配列に変更

--- database/seeders/Prefecture_categoriesSeeder.php
+++ database/seeders/Prefecture_categoriesSeeder.php
@@ -16,238 +16,238 @@ class Prefecture_categoriesSeeder extends Seeder
     {
         //
         DB::table('prefecture_categories')->insert([
-            'name'=>'愛知県',
-            'sort_order'=>1,
+            ['name'=>'愛知県'],
+            ['sort_order'=>1],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'青森県',
-            'sort_order'=>2,
+            ['name'=>'青森県'],
+            ['sort_order'=>2],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'秋田県',
-            'sort_order'=>3,
+            ['name'=>'秋田県'],
+            ['sort_order'=>3],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'石川県',
-            'sort_order'=>4
+            ['name'=>'石川県'],
+            ['sort_order'=>4],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'茨城県',
-            'sort_order'=>5,
+            ['name'=>'茨城県'],
+            ['sort_order'=>5],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'岩手県',
-            'sort_order'=>6,
+            ['name'=>'岩手県'],
+            ['sort_order'=>6],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'愛媛県',
-            'sort_order'=>7,
+            ['name'=>'愛媛県'],
+            ['sort_order'=>7],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'大分県',
-            'sort_order'=>8,
+            ['name'=>'大分県'],
+            ['sort_order'=>8],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'大阪府',
-            'sort_order'=>9,
+            ['name'=>'大阪府'],
+            ['sort_order'=>9],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'岡山県',
-            'sort_order'=>10,
+            ['name'=>'岡山県'],
+            ['sort_order'=>10],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'沖縄県',
-            'sort_order'=>11,
+            ['name'=>'沖縄県'],
+            ['sort_order'=>11],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'香川県',
-            'sort_order'=>12,
+            ['name'=>'香川県'],
+            ['sort_order'=>12],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'鹿児島県',
-            'sort_order'=>13,
+            ['name'=>'鹿児島県'],
+            ['sort_order'=>13],
         ]);;
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'神奈川県',
-            'sort_order'=>14,
+            ['name'=>'神奈川県'],
+            ['sort_order'=>14],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'岐阜県',
-            'sort_order'=>15,
+            ['name'=>'岐阜県'],
+            ['sort_order'=>15],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'京都府',
-            'sort_order'=>16,
+            ['name'=>'京都府'],
+            ['sort_order'=>16],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'熊本県',
-            'sort_order'=>17,
+            ['name'=>'熊本県'],
+            ['sort_order'=>17],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'群馬県',
-            'sort_order'=>18,
+            ['name'=>'群馬県'],
+            ['sort_order'=>18],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'高知県',
-            'sort_order'=>19,
+            ['name'=>'高知県'],
+            ['sort_order'=>19],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'埼玉県',
-            'sort_order'=>20,
+            ['name'=>'埼玉県'],
+            ['sort_order'=>20],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'佐賀県',
-            'sort_order'=>21,
+            ['name'=>'佐賀県'],
+            ['sort_order'=>21],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'滋賀県',
-            'sort_order'=>22,
+            ['name'=>'滋賀県'],
+            ['sort_order'=>22],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'静岡県',
-            'sort_order'=>23,
+            ['name'=>'静岡県'],
+            ['sort_order'=>23],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'島根県',
-            'sort_order'=>24,
+            ['name'=>'島根県'],
+            ['sort_order'=>24],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'千葉県',
-            'sort_order'=>25,
+            ['name'=>'千葉県'],
+            ['sort_order'=>25],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'東京都',
-            'sort_order'=>26,
+            ['name'=>'東京都'],
+            ['sort_order'=>26],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'徳島県',
-            'sort_order'=>27,
+            ['name'=>'徳島県'],
+            ['sort_order'=>27],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'栃木県',
-            'sort_order'=>28,
+            ['name'=>'栃木県'],
+            ['sort_order'=>28],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'鳥取県',
-            'sort_order'=>29,
+            ['name'=>'鳥取県'],
+            ['sort_order'=>29],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'富山県',
-            'sort_order'=>30,
+            ['name'=>'富山県'],
+            ['sort_order'=>30],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'長崎県',
-            'sort_order'=>31,
+            ['name'=>'長崎県'],
+            ['sort_order'=>31],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'長野県',
-            'sort_order'=>32,
+            ['name'=>'長野県'],
+            ['sort_order'=>32],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'奈良県',
-            'sort_order'=>33,
+            ['name'=>'奈良県'],
+            ['sort_order'=>33],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'新潟県',
-            'sort_order'=>34,
+            ['name'=>'新潟県'],
+            ['sort_order'=>34],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'兵庫県',
-            'sort_order'=>35,
+            ['name'=>'兵庫県'],
+            ['sort_order'=>35],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'広島県',
-            'sort_order'=>36,
+            ['name'=>'広島県'],
+            ['sort_order'=>36],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'福井県',
-            'sort_order'=>37,
+            ['name'=>'福井県'],
+            ['sort_order'=>37],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'福岡県',
-            'sort_order'=>38,
+            ['name'=>'福岡県'],
+            ['sort_order'=>38],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'福島県',
-            'sort_order'=>39,
+            ['name'=>'福島県'],
+            ['sort_order'=>39],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'北海道',
-            'sort_order'=>40,
+            ['name'=>'北海道'],
+            ['sort_order'=>40],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'三重県',
-            'sort_order'=>41,
+            ['name'=>'三重県'],
+            ['sort_order'=>41],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'宮城県',
-            'sort_order'=>42,
+            ['name'=>'宮城県'],
+            ['sort_order'=>42],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'宮崎県',
-            'sort_order'=>43,
+            ['name'=>'宮崎県'],
+            ['sort_order'=>43],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'山形県',
-            'sort_order'=>44,
+            ['name'=>'山形県'],
+            ['sort_order'=>44],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'山口県',
-            'sort_order'=>45,
+            ['name'=>'山口県'],
+            ['sort_order'=>45],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'山梨県',
-            'sort_order'=>46,
+            ['name'=>'山梨県'],
+            ['sort_order'=>46],
         ]);
 
         DB::table('prefecture_categories')->insert([
-            'name'=>'和歌山県',
-            'sort_order'=>47,
+            ['name'=>'和歌山県'],
+            ['sort_order'=>47],
         ]);
     }
 }
