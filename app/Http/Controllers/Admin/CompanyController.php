a48eea3661dc9dd2d825d7f8931b35187a50d22e
kitaura <s.kitaura@core-tech.jp>
2025-05-29 10:36:21 +0900

    CompanyControllerにindex functionを追加

--- app/Http/Controllers/Admin/CompanyController.php
+++ app/Http/Controllers/Admin/CompanyController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\companies;
+use App\Models\Company;
 
 class CompanyController extends Controller
 {
     //企業一覧の表示
     public function index(){
+        $companies = companies::where('is_recruiting', 1)
+                        ->select('id', 'name', 'prefecture_id')
+                        ->get();
 
+        return view('admin.companies.index', compact('companies'));
     }
 
 }
