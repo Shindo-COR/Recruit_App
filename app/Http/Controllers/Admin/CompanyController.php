ab44be061e751863cd6c1adc3e27a2bb76916d7a
kitaura <s.kitaura@core-tech.jp>
2025-05-29 10:42:16 +0900

    モデル名の統一

--- app/Http/Controllers/Admin/CompanyController.php
+++ app/Http/Controllers/Admin/CompanyController.php
@@ -9,7 +9,7 @@ class CompanyController extends Controller
 {
     //企業一覧の表示
     public function index(){
-        $companies = companies::where('is_recruiting', 1)
+        $companies = Company::where('is_recruiting', 1)
                         ->select('id', 'name', 'prefecture_id')
                         ->get();
 
